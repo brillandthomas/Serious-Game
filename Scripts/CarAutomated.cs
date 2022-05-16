7f16720b42a1573b1a81c9af1530d361582a9726
Firas357951 <firas.jandoubi@student-cs.fr>
2022-05-16 14:32:09 +0200

    Version 16/05

--- Scripts/CarAutomated.cs
+++ Scripts/CarAutomated.cs
@@ -31,10 +31,40 @@ public class CarAutomated : MonoBehaviour
         transform.position += transform.forward * speed * Time.deltaTime;
     }
 
-
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.gameObject.GetComponent<TrafficLight>() != null)
-            isMoving = false;
+        {
+            TrafficLight trafficLightObject = other.gameObject.GetComponent<TrafficLight>();
+            if (trafficLightObject.greenLight) 
+                isMoving = true;
+            else 
+                isMoving = false;
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.GetComponent<CarAutomated>() != null)
+        {
+            CarAutomated CarObject = other.gameObject.GetComponent<CarAutomated>();
+
+            if (CarObject.isMoving)
+            {
+                speed = CarObject.speed;
+            }
+            else
+            {
+                isMoving = false;
+            }
+        }
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CarAutomated>() != null)
+            isMoving = true;
+
     }
 }
--- Scripts/TrafficLight.cs
+++ Scripts/TrafficLight.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrafficLight : MonoBehaviour
-{
+{   
+    public bool greenLight = false;
+    public float duree_redlight = 10;
+    public float duree_greenlight = 10;
+    public float last_update = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +17,22 @@ public class TrafficLight : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        if (greenLight)
+        {
+            if (Time.fixedTime > duree_redlight + last_update)
+            {
+                greenLight = false;
+                last_update = Time.fixedTime;
+            }
+        }
+        else
+        {
+            if (Time.fixedTime > duree_greenlight + last_update)
+            {
+                greenLight = true;
+                last_update = Time.fixedTime;
+            }
+        }
     }
 }
